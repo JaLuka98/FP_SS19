921298d777b0dda6ab6fcee6d0423fbd9853798d
Jan Lukas Schubert <jan-lukas.schubert@tu-dortmund.de>
2019-04-09 11:24:37 +0200

    Added data. Some additional information is IN the .txt files

--- V53_mikrowellen/plot.py
+++ V53_mikrowellen/plot.py
@@ -5,3 +5,13 @@ import uncertainties.unumpy as unp
 from scipy import optimize
 import matplotlib.pyplot as plt
 from uncertainties import ufloat
+
+#Frequenzmessung
+f_dip=9006 #MHz
+
+#3dB Mehtode
+d1=69.6 #mm
+d2=67.9
+
+#Abschwächer Methode
+A_3=42  #dB
