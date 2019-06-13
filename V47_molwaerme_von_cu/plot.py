620e0f85313065b306e3322de1f027d2c43e7011
Jan Lukas Schubert <jan-lukas.schubert@tu-dortmund.de>
2019-06-13 15:05:27 +0200

    Korrektur

--- V47_molwaerme_von_cu/plot.py
+++ V47_molwaerme_von_cu/plot.py
@@ -26,17 +26,27 @@ C_V = np.array([0.0555, 0.225, 0.462, 0.963, 1.693, 2.64, 3.74, 6.15])
 n=5.382 #mol. Folgt aus n=m/M mit der Masse der Probe und der molaren Masse von Cu
 
 Rp, Rz=np.genfromtxt('data/data.txt', unpack=True)
+Rp = unp.uarray(Rp, 0.1)
+Rz = unp.uarray(Rz, 0.1)
 I, t, U=np.genfromtxt('data/data2.txt', unpack=True)
-I41=np.append(I, 0)
-t41=np.append(t, 0)
-U41=np.append(U, 0)
+I=unp.uarray(I, 0.1)
+t=unp.uarray(t, 3)
+U=unp.uarray(U, 0.1)
+I41=np.append(0, I)
+t41=np.append(0, t)
+U41=np.append(0, U)
 hr = ['$R_p/\Omega$', '$R_z/\Omega$','$I$/µA', '$t$/s', '$U/$V']
-m = np.zeros((41, 5))
-m[:,0] = Rp
-m[:,1] = Rz
-m[:,2] = I41
-m[:,3] = t41
-m[:,4] = U41
+m = np.zeros((41, 10))
+m[:,0] = unp.nominal_values(Rp)
+m[:,1] = unp.std_devs(Rp)
+m[:,2] = unp.nominal_values(Rz)
+m[:,3] = unp.std_devs(Rz)
+m[:,4] = unp.nominal_values(I41)
+m[:,5] = unp.std_devs(I41)
+m[:,6] = unp.nominal_values(t41)
+m[:,7] = unp.std_devs(t41)
+m[:,8] = unp.nominal_values(U41)
+m[:,9] = unp.std_devs(U41)
 t=matrix2latex(m, headerRow=hr, format='%.2f')
 print(t)
 
@@ -44,6 +54,10 @@ Tp=0.00134*Rp**2+2.296*Rp -243.02 +273.15
 Tz=0.00134*Rz**2+2.296*Rz -243.02 +273.15
 I, t, U=np.genfromtxt('data/data2.txt', unpack=True)
 I*=1e-3 #jetzt in Ampere
+I=unp.uarray(I, 0.1)
+t=unp.uarray(t, 3)
+U=unp.uarray(U, 0.1)
+
 E=I*U*t
 
 DeltaT=[]
@@ -54,16 +68,20 @@ for i in range(1, 41):
 
 C_p=E/(n*np.asarray(DeltaT))
 
-C_p41=np.append(C_p, 0)
-DeltaT41=np.append(DeltaT, 0)
-E41=np.append(E, 0)
+C_p41=np.append(0,C_p)
+DeltaT41=np.append(0,DeltaT)
+E41=np.append(0,E)
 
 hr = ['$T_p/$K', '$\Delta T_p/$K','$E$/J', '$C_p$/\frac{mol}{kg K}']
-m = np.zeros((41, 4))
-m[:,0] = Tp
-m[:,1] = DeltaT41
-m[:,2] = E41
-m[:,3] = C_p41
+m = np.zeros((41, 8))
+m[:,0] = unp.nominal_values(Tp)
+m[:,1] = unp.std_devs(Tp)
+m[:,2] = unp.nominal_values(DeltaT41)
+m[:,3] = unp.std_devs(DeltaT41)
+m[:,4] = unp.nominal_values(E41)
+m[:,5] = unp.std_devs(E41)
+m[:,6] = unp.nominal_values(C_p41)
+m[:,7] = unp.std_devs(C_p41)
 t=matrix2latex(m, headerRow=hr, format='%.2f')
 print(t)
 
@@ -100,7 +118,7 @@ alpha_T=a/Tp+b
 
 C_v=C_p41-alpha_T**2*kappa*V0*Tp
 
-hr = ['$T$/K', '$\alpha$/10^-6 °'] #hier vielleicht noch die werte für C_v rein?
+hr = ['$T$/K', '$\alpha$/10^-6 °']
 m = np.zeros((24, 2))
 m[:,0] = T
 m[:,1] = alpha*1e6
@@ -114,7 +132,8 @@ m[:,1] = unp.std_devs(C_v)
 t=matrix2latex(m, headerRow=hr, format='%.5f')
 print(t)
 
-plt.errorbar(Tp, unp.nominal_values(C_v), fmt='rx',mew=0.5, label='Errechnete Werte') #Irgendwas klappt hier mit den errorbars noch nicht so richtig
+
+plt.errorbar(unp.nominal_values(Tp[1:41]), unp.nominal_values(C_v[1:41]), yerr=unp.std_devs(C_v[1:41]), fmt='rx',mew=0.5, ecolor='b', elinewidth=0.5,  label='Errechnete Werte') #Irgendwas klappt hier mit den errorbars noch nicht so richtig
 plt.xlabel(r'$T/$K')
 plt.ylabel(r'$C_v$ in J/(mol K)')
 #plt.axis([50,320,0,0.00002])
@@ -128,7 +147,7 @@ plt.clf()
 
 #Dieses komische Debye Zeug
 
-params, covariance_matrix = optimize.curve_fit(debyeFunction, Tp[0:20], unp.nominal_values(C_v[0:20]))
+params, covariance_matrix = optimize.curve_fit(debyeFunction, unp.nominal_values(Tp[1:21]), unp.nominal_values(C_v[1:21]), sigma=unp.std_devs(C_v[1:21]), absolute_sigma=True)
 errors = np.sqrt(np.diag(covariance_matrix))
 theta_D = params[0]
 sigma_theta_D = errors[0]
@@ -137,9 +156,10 @@ print("theta_D = ", theta_D_ufloat)
 
 Tlin = np.linspace(-1, 180, 200)
 C_Vplot = debyeFunction(Tlin, theta_D)
-plt.plot(Tp[0:20], unp.nominal_values(C_v[0:20]), 'rx', mew=0.5, label='Messwerte')
-plt.plot(Tlin, C_Vplot, 'b-', label='Ausgleichsfunktion', linewidth=0.5)
+plt.errorbar(unp.nominal_values(Tp[1:21]), unp.nominal_values(C_v[1:21]), yerr=unp.std_devs(C_v[1:21]), fmt='rx',mew=0.5, ecolor='b', elinewidth=0.5, label='Messwerte')
+plt.plot(Tlin, C_Vplot, 'g-', label='Ausgleichsfunktion', linewidth=0.5)
 plt.grid()
+plt.legend()
 plt.axis([0, 180, -1, 25])
 plt.xlabel(r'$T$/K')
 plt.ylabel(r'$C_V/$(J/mol K)')
